135352d3fed335041ab2440a1feb9eba05a7d2a1
davesmith00000 <davesmith.00000@gmail.com>
2025-03-09 16:21:03 +0000

    TerminalInput example

--- examples/terminal-ui/input/src/InputExample.scala
+++ examples/terminal-ui/input/src/InputExample.scala
@@ -3,86 +3,25 @@ package indigoexamples
 import indigo.*
 import indigoextras.ui.*
 import indigoextras.ui.syntax.*
-
+import roguelikestarterkit.*
+import roguelikestarterkit.syntax.*
+import roguelikestarterkit.ui.*
 import generated.*
 
 import scala.scalajs.js.annotation.*
 
 object CustomComponents:
 
-  private val text =
-    Text("", DefaultFont.fontKey, Assets.assets.generated.DefaultFontMaterial)
-
-  private def drawCursor(ctx: UIContext[Unit], input: Input[Unit]): Batch[SceneNode] =
-    val pt =
-      ctx.services.bounds
-        .get(
-          text.withText(input.text.take(input.cursor.position))
-        )
-        .topRight + Point(0, -4)
-
-    Batch(
-      Shape.Box(
-        Rectangle(ctx.parent.coords.unsafeToPoint + pt, Size(2, 20)),
-        Fill.Color(RGBA.Green)
-      )
+  val charSheet: CharSheet =
+    CharSheet(
+      Assets.assets.AnikkiSquare10x10,
+      Size(10),
+      RoguelikeTiles.Size10x10.charCrops,
+      RoguelikeTiles.Size10x10.Fonts.fontKey
     )
 
-  private def present: (UIContext[Unit], Input[Unit]) => Outcome[Layer] =
-    (ctx, input) =>
-      val cursor: Batch[SceneNode] =
-        if input.hasFocus then
-          input.cursor.blinkRate match
-            case None =>
-              drawCursor(ctx, input)
-
-            case Some(blinkRate) =>
-              Signal
-                .Pulse(blinkRate)
-                .map(p =>
-                  if (ctx.frame.time.running - input.cursor.lastModified < Seconds(0.5)) true else p
-                )
-                .map {
-                  case false =>
-                    Batch.empty
-
-                  case true =>
-                    drawCursor(ctx, input)
-                }
-                .at(ctx.frame.time.running)
-        else Batch.empty
-
-      val border =
-        if input.hasFocus then
-          Batch(
-            Shape
-              .Box(
-                ctx.parent.bounds.unsafeToRectangle,
-                Fill.None,
-                Stroke(1, RGBA.Green)
-              )
-              .moveTo(ctx.parent.coords.unsafeToPoint)
-          )
-        else Batch.empty
-
-      Outcome(
-        Layer(
-          Batch(
-            text
-              .withText(input.text)
-              .moveTo(ctx.parent.coords.unsafeToPoint)
-          ) ++ cursor ++ border
-        )
-      )
-
   val component: Input[Unit] =
-    Input(
-      Dimensions(200, 40),
-      (ctx: UIContext[Unit], inputText: String) =>
-        ctx.services.bounds.get(text.withText(inputText)).width
-    )(present)
-      .withText("Hello, world!")
-      .withCursorBlinkRate(Seconds(0.3))
+    TerminalInput[Unit]("Hello, world!", 20, TerminalInput.Theme(charSheet))
 
 final case class Model(component: Input[Unit])
 object Model:
@@ -95,17 +34,18 @@ object Model:
 final case class Log(message: String) extends GlobalEvent
 
 @JSExportTopLevel("IndigoGame")
-object HitAreaExample extends IndigoSandbox[Unit, Model]:
+object InputExample extends IndigoSandbox[Unit, Model]:
 
   val config: GameConfig =
     Config.config.noResize
+      .withMagnification(2)
 
   val assets: Set[AssetType] =
-    Assets.assets.assetSet ++ Assets.assets.generated.assetSet
+    Assets.assets.assetSet
 
-  val fonts: Set[FontInfo]        = Set(DefaultFont.fontInfo)
+  val fonts: Set[FontInfo]        = Set()
   val animations: Set[Animation]  = Set()
-  val shaders: Set[ShaderProgram] = Set()
+  val shaders: Set[ShaderProgram] = roguelikestarterkit.shaders.all
 
   def setup(assetCollection: AssetCollection, dice: Dice): Outcome[Startup[Unit]] =
     Outcome(Startup.Success(()))
@@ -120,7 +60,12 @@ object HitAreaExample extends IndigoSandbox[Unit, Model]:
 
     case e =>
       val ctx = UIContext(context)
-        .moveParentBy(Coords(50, 50))
+        .withSnapGrid(CustomComponents.charSheet.size)
+        .moveParentBy(Coords(5, 5))
+        .withPointerCoords(
+          Coords(context.frame.input.pointers.position / CustomComponents.charSheet.size.toPoint)
+        )
+        .withMagnification(2)
 
       model.component.update(ctx)(e).map { c =>
         model.copy(component = c)
@@ -128,7 +73,12 @@ object HitAreaExample extends IndigoSandbox[Unit, Model]:
 
   def present(context: Context[Unit], model: Model): Outcome[SceneUpdateFragment] =
     val ctx = UIContext(context)
-      .moveParentBy(Coords(50, 50))
+      .withSnapGrid(CustomComponents.charSheet.size)
+      .moveParentBy(Coords(5, 5))
+      .withPointerCoords(
+        Coords(context.frame.input.pointers.position / CustomComponents.charSheet.size.toPoint)
+      )
+      .withMagnification(2)
 
     model.component
       .present(ctx)
