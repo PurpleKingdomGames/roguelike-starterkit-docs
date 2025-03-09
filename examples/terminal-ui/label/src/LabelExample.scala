b6d1dd8874111c875900442194375fe344bff352
davesmith00000 <davesmith.00000@gmail.com>
2025-03-09 15:57:54 +0000

    Terminal Button and Label demos

--- examples/terminal-ui/label/src/LabelExample.scala
+++ examples/terminal-ui/label/src/LabelExample.scala
@@ -1,64 +1,32 @@
 package indigoexamples
 
-/** ## How to set up a custom label
-  *
-  * ### Imports
-  */
-
 import indigo.*
 
-// Before we do anything else, we'll need some additional imports:
-// ``` scala
 import indigoextras.ui.*
 import indigoextras.ui.syntax.*
-// ```
-
+import roguelikestarterkit.*
+import roguelikestarterkit.syntax.*
+import roguelikestarterkit.ui.*
 import generated.*
 
 import scala.scalajs.js.annotation.*
 
-/** ### Defining a custom label
-  *
-  * To keep the code nice and tidy, we'll define our custom label in a separate object.
-  *
-  * To render the label we're going to use a TextBox. TextBox is not a great choice in practice,
-  * because it is relatively expensive, and to calculate the bounds for dynamic text we'll need to
-  * find a way to supply a `Context.Services.Bounds` instance. But for this demo, it'll do.
-  *
-  * Text in labels can be static, or dynamic based on the 'reference' data in the `UIContext`, as in
-  * the example below.
-  */
-// ``` scala
 object CustomComponents:
 
-  val text =
-    Text(
-      "",
-      DefaultFont.fontKey,
-      Assets.assets.generated.DefaultFontMaterial
+  val charSheet: CharSheet =
+    CharSheet(
+      Assets.assets.AnikkiSquare10x10,
+      Size(10),
+      RoguelikeTiles.Size10x10.charCrops,
+      RoguelikeTiles.Size10x10.Fonts.fontKey
     )
 
   val customLabel: Label[Int] =
-    Label[Int](
+    TerminalLabel(
       ctx => "Count: " + ctx.reference,
-      (ctx, label) => Bounds(ctx.services.bounds.get(text.withText(label)))
-    ) { case (ctx, label) =>
-      Outcome(
-        Layer(
-          text
-            .withText(label.text(ctx))
-            .moveTo(ctx.parent.coords.unsafeToPoint)
-        )
-      )
-    }
-// ```
+      TerminalLabel.Theme(charSheet, RGBA.Magenta, RGBA.Cyan)
+    )
 
-/** ### Setting up the Model
-  *
-  * Here we initialise our model with our label, and the 'count', which will be injected as the
-  * reference data, later.
-  */
-// ``` scala
 final case class Model(count: Int, label: Label[Int])
 object Model:
 
@@ -67,23 +35,20 @@ object Model:
       42,
       CustomComponents.customLabel
     )
-// ```
 
 @JSExportTopLevel("IndigoGame")
 object LabelExample extends IndigoSandbox[Unit, Model]:
 
   val config: GameConfig =
     Config.config.noResize
+      .withMagnification(2)
 
-  // We need to register the font assets and font info for the `Text` instance to work.
-  // ```scala
   val assets: Set[AssetType] =
-    Assets.assets.assetSet ++ Assets.assets.generated.assetSet
+    Assets.assets.assetSet
 
-  val fonts: Set[FontInfo] = Set(DefaultFont.fontInfo)
-  // ```
+  val fonts: Set[FontInfo]        = Set()
   val animations: Set[Animation]  = Set()
-  val shaders: Set[ShaderProgram] = Set()
+  val shaders: Set[ShaderProgram] = roguelikestarterkit.shaders.all
 
   def setup(assetCollection: AssetCollection, dice: Dice): Outcome[Startup[Unit]] =
     Outcome(Startup.Success(()))
@@ -91,35 +56,31 @@ object LabelExample extends IndigoSandbox[Unit, Model]:
   def initialModel(startupData: Unit): Outcome[Model] =
     Outcome(Model.initial)
 
-  /** ### Updating the Model
-    *
-    * We need to construct a `UIContext` to pass to the component group, and then we can update the
-    * label by supply it with the context and the event.
-    */
-  // ``` scala
   def updateModel(context: Context[Unit], model: Model): GlobalEvent => Outcome[Model] =
     case e =>
       val ctx = UIContext(context)
-        .moveParentBy(Coords(50, 50))
+        .withSnapGrid(CustomComponents.charSheet.size)
+        .moveParentBy(Coords(5, 5))
+        .withPointerCoords(
+          Coords(context.frame.input.pointers.position / CustomComponents.charSheet.size.toPoint)
+        )
+        .withMagnification(2)
         .copy(reference = model.count)
 
       model.label.update(ctx)(e).map { l =>
         model.copy(label = l)
       }
-  // ```
 
-  /** ### Presenting the Label
-    *
-    * We need to call the `present` method with, once again, and instance of UIContext, and provide
-    * the results to a SceneUpdateFragment.
-    */
-  // ``` scala
   def present(context: Context[Unit], model: Model): Outcome[SceneUpdateFragment] =
     val ctx = UIContext(context)
-      .moveParentBy(Coords(50, 50))
+      .withSnapGrid(CustomComponents.charSheet.size)
+      .moveParentBy(Coords(5, 5))
+      .withPointerCoords(
+        Coords(context.frame.input.pointers.position / CustomComponents.charSheet.size.toPoint)
+      )
+      .withMagnification(2)
       .copy(reference = model.count)
 
     model.label
       .present(ctx)
       .map(l => SceneUpdateFragment(l))
-  // ```
