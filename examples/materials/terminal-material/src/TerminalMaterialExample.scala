6b5183603d8f12921257d8ce66d137f6e4347675
davesmith00000 <davesmith.00000@gmail.com>
2026-09-02 08:24:16 +0100

    Bump to 0.30.0-M6 & Mill 1.1.8

--- examples/materials/terminal-material/src/TerminalMaterialExample.scala
+++ examples/materials/terminal-material/src/TerminalMaterialExample.scala
@@ -4,9 +4,6 @@ import indigo.*
 import generated.Config
 import generated.Assets
 import roguelikestarterkit.*
-import ultraviolet.syntax.*
-
-import scala.annotation.nowarn
 
 final case class Model()
 object Model:
@@ -24,7 +21,7 @@ class TerminalMaterialExample() extends Game[Unit, Unit, Model]:
         .noData(Config.config)
         .withAssets(Assets.assets.assetSetRelative)
         .withFonts(RoguelikeTiles.Size10x10.Fonts.fontInfo)
-        .withShaders(roguelikestarterkit.shaders.all ++ Set(CustomShader.shader))
+        .withShaders(roguelikestarterkit.shaders.all)
     )
 
   def scenes(bootData: Unit): NonEmptyBatch[Scene[Model]] =
@@ -66,12 +63,6 @@ class TerminalMaterialExample() extends Game[Unit, Unit, Model]:
           Text(
             message,
             RoguelikeTiles.Size10x10.Fonts.fontKey,
-            TerminalMaterial(Assets.assets.AnikkiSquare10x10, RGBA.Yellow, RGBA.Red)
-              .withShaderId(CustomShader.shader.id)
-          ).moveBy(0, 40),
-          Text(
-            message,
-            RoguelikeTiles.Size10x10.Fonts.fontKey,
             TerminalMaterial(
               Assets.assets.AnikkiSquare10x10,
               RGBA.White,
@@ -82,51 +73,3 @@ class TerminalMaterialExample() extends Game[Unit, Unit, Model]:
         )
       ).withMagnification(Magnification.x2)
     )
-
-object CustomShader:
-
-  val shader: UltravioletShader =
-    UltravioletShader.entityFragment(
-      ShaderId("custom terminal text shader"),
-      EntityShader.fragment[CustomShader.Env](
-        CustomShader.frag,
-        CustomShader.Env.ref
-      )
-    )
-
-  final case class Env(
-      FOREGROUND: vec3,
-      BACKGROUND: vec4,
-      MASK: vec4
-  ) extends FragmentEnvReference
-
-  object Env:
-    val ref =
-      Env(vec3(0.0f), vec4(0.0f), vec4(0.0f))
-
-  final case class RogueLikeTextData(
-      FOREGROUND: vec3,
-      BACKGROUND: vec4,
-      MASK: vec4
-  )
-
-  @nowarn("msg=unused")
-  inline def frag: Shader[Env, Unit] =
-    Shader[Env] { env =>
-      ubo[RogueLikeTextData]
-
-      def fragment(color: vec4): vec4 =
-        val maskDiff: Boolean = abs(env.CHANNEL_0.x - env.MASK.x) < 0.001f &&
-          abs(env.CHANNEL_0.y - env.MASK.y) < 0.001f &&
-          abs(env.CHANNEL_0.z - env.MASK.z) < 0.001f &&
-          abs(env.CHANNEL_0.w - env.MASK.w) < 0.001f
-
-        val c: vec4 =
-          if (maskDiff) {
-            env.BACKGROUND
-          } else {
-            vec4(env.CHANNEL_0.rgb * (env.FOREGROUND.rgb * env.CHANNEL_0.a), env.CHANNEL_0.a)
-          }
-
-        c * (1.0f - (vec4(env.SCREEN_COORDS.x) / 250.0f)) // Example custom mod
-    }
